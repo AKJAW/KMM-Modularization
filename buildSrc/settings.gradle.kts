2423dc48cdf0c69d1e5fe2627d0ea125995049a1
AKJAW <akjaworski1@gmail.com>
2023-05-02 16:54:48 +0200

    chore: refresh version

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,6 +1,12 @@
 pluginManagement {
     plugins {
         id("de.fayard.refreshVersions") version "0.40.0"
+////                                # available:"0.40.1"
+////                                # available:"0.40.2"
+////                                # available:"0.50.0"
+////                                # available:"0.50.1"
+////                                # available:"0.50.2"
+////                                # available:"0.51.0"
     }
 }
 
